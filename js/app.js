1fcb1c8d2588a2eaf8649630704fb2e84a5c41a0
mason <mwalker0909>
2019-10-07 23:08:23 -0700

    broken code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 'use strict';
 //global variables
-var productEntries = {};
+
 var productFiles = ['1.jpg', 'banana.jpg', 'boots.jpg', 'breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
 var counter = 0 
 
+productOne = null;
+productTwo = null;
+productThree = null;
 
 
 // construction function
@@ -12,33 +15,56 @@ function product(name, filePath) {
   this.filePath = filePath;
   this.sumviews = 0
   this.sumclicks = 0
-  product.allProducts.push(this);
+  product.allImages.push(this);
 
 };
 
-product.allProducts= [];
+product.allImages= [];
 
 
 // creating array for products
 
 var renderProducts = function (productOne, productTwo, productThree) {
 
-  leftProduct.src = product.allProducts[productOne].filePath;
-  centerProduct.src = product.allProducts[productTwo].filePath;
-  rightProduct.src = product.allProducts[productThree].filePath;
+  leftProduct.src = product.allImages[productOne].filePath;
+  centerProduct.src = product.allImages[productTwo].filePath;
+  rightProduct.src = product.allImages[productThree].filePath;
 }
 
-// Random generated number
-var getRandomProduct = function(){
-  return Math.floor(Math.random() * Product.allProducts.length +1);
+//  Random generated number
+var getRandomProduct = function() {
+
+var productOne = Math.ceil(Math.random() * Product.allImages.length -1);
+  do {
+    var productTwo = Math.ceil(Math.random() * Product.allProducts.length -1);
+  } while (productOne !== productTwo);
+
+  do { 
+    var productThree = Math.ceil(Math.random() * Product.allProducts.length -1);
+  } while (productThree !== productOne || projectTwo);
 }
 
-//choose product 
-var chooseProduct = function () {
-  var productOne = getRandomProduct ();
 
-}
 
+
+
+
+
+
+// var chooseProduct = function () {
+// productOne = getRandomProduct ();
+// productTwo = getRandomProduct ();
+// productThree = getRandomProduct ();
+//  if (productOne === productTwo || productThree) {
+//    chooseProduct();
+//   if (productTwo === productOne|| productTwo) {
+//    chooseProduct();
+//   if (productThree === productTwo || productOne) {
+//   chooseProduct();
+//  }
+// }
+
+renderProducts();
 
 //event handler
 var clickProduct = function (event) {
@@ -59,6 +85,5 @@ var clickProduct = function (event) {
       pickNewProducts();
   }
 }
-  imageSectionTag.addEventListener('click', clickProduct);
-
-}
+  imageSectionTag.addEventListener('click', clickProduct)
+  }
